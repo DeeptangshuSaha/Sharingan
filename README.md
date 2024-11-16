9d571fe0ef3132752baae372018662838b3cd9cc
LichtBach <deeptangshusaha62@gmail.com>
2024-11-16 21:14:56 +0530

    Some updates

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 ---
 
 This is a Basic Facial-Recognition Program.
-
+It also has the capability to identify emotions.
 It uses the face-recogniton module for its facial recognition purposes.
 
 ___
--- source code/main_video.py
+++ source code/main_video.py
@@ -71,7 +71,7 @@ while True:
         result = DeepFace.analyze(face_roi, actions=['emotion'], enforce_detection=False)
         emot = result[0]['dominant_emotion']
         cv2.putText(frame, name, (x1, y1 - 10), cv2.FONT_HERSHEY_DUPLEX, 1, (0, 0, 200), 2)
-        cv2.putText(frame, emot, (x1, x2 - 10), cv2.FONT_HERSHEY_DUPLEX, 1, (0, 0, 200), 2)
+        cv2.putText(frame, emot, (x1, y1 + 10), cv2.FONT_HERSHEY_DUPLEX, 1, (0, 0, 200), 2)
         cv2.rectangle(frame, (x1, y1), (x2, y2), (200, 0, 0), 4)
 
         # cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 0, 255), 4)
